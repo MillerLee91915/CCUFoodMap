0957acb8b1fb07cc09e73636759525cce2ce703d
Miller <miller0809@gmail.com>
2021-09-09 14:25:02 +0800

    123

---
+++ app/src/main/java/eu/tutorial/ccufoodmap/MainActivity.kt
@@ -0,0 +1,47 @@
+package eu.tutorial.ccufoodmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import eu.tutorial.ccufoodmap.fragments.HomeFragment
+import eu.tutorial.ccufoodmap.fragments.ListFragment
+import eu.tutorial.ccufoodmap.fragments.SettingFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // var for db
+        val filename=filesDir.toString()+"/foodList.db"
+        val tv_QueryResult = findViewById<TextView>(R.id.textview_query_result)
+        val btn_create_database_table = findViewById<Button>(R.id.button_create_database)
+
+        //var for buttom nav bar
+        val homeFragment :Fragment= HomeFragment()
+        val listFragment :Fragment= ListFragment()
+        val settingFragment :Fragment= SettingFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        val bottom_nav: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottom_nav.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_food -> makeCurrentFragment(listFragment)
+                R.id.ic_setting -> makeCurrentFragment(settingFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
+}
